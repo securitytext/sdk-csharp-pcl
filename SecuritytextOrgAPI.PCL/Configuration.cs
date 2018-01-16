83d9276bd35f54cfbeffae93f3a94eab480ee1a9
securitytext <me@austinheap.com>
2018-01-16 18:13:03 +0000

    initial commit

---
+++ SecuritytextOrgAPI.PCL/Configuration.cs
@@ -0,0 +1,11 @@
+using SecuritytextOrgAPI.PCL.Models;
+namespace SecuritytextOrgAPI.PCL
+{
+    public partial class Configuration
+    {
+
+        //The base Uri for API calls
+        public static string BaseUri = "https://api.securitytext.org/v1";
+
+    }
+}
---
+++ SecuritytextOrgAPI.PCL/ISecuritytextOrgAPIClient.cs
@@ -0,0 +1,20 @@
+/*
+ * SecuritytextOrgAPI.PCL
+ *
+ * This file was automatically generated for SecurityTextOrg by APIMATIC v2.0 ( https://apimatic.io )
+ */
+using System;
+using SecuritytextOrgAPI.PCL.Controllers;
+
+namespace SecuritytextOrgAPI.PCL
+{
+    public partial interface ISecuritytextOrgAPIClient
+    {
+
+        /// <summary>
+        /// Singleton access to Whois controller
+        /// </summary>
+        IWhoisController Whois { get;}
+
+    }
+}
